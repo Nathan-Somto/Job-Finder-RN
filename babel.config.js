040316f3092ff15b241866a382850d7252cbb036
Mkparu <mkparusomtochi26@gmail.com>
2023-07-19 15:26:56 +0100

    chore(aliases): add aliases for improved module import.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          root: ['./app'],
+          '@components': './app/components',
+          '@screens': './app/screens',
+          '@utils': './app/utils',
+          '@navigation': './app/navigation',
+          '@constants': './app/constants',
+          '@data': './app/data',
+          '@assets': './assets'
+        }
+      }],
+      'react-native-reanimated/plugin'
+    ]
+  }
+}
